bcbbbf8a9416efc8acbd7fee3ec49aa22928625a
Bun Bun <Aaronrls1984@gmail.com>
2022-11-06 20:48:44 -0500

    again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 // Connecting mongoDB Database
 mongoose
-  .connect(process.env.MONGODB_URI||'mongodb+srv://test:test@cluster0.nfmkma3.mongodb.net/?retryWrites=true&w=majority')
+  .connect(process.env.MONGODB_URL||'mongodb+srv://test:test@cluster0.nfmkma3.mongodb.net/?retryWrites=true&w=majority')
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -17,22 +17,32 @@ mongoose
     console.error('Error connecting to mongo', err.reason)
   })
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+
+}
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(cors());
 app.use('/students', studentRoute)
-app.use(express.static(path.join(__dirname, './build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build'))
-})
 
-if (process.env.NODE_ENV==='production'){
-  app.use(express.static('build'))
-}
+
+
+
 // PORT
 const port = process.env.PORT || 4000;
+
+
 const server = app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
