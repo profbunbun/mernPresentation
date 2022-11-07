5747cff0c41fd6b94beab0ccbd4918f042eebde7
Bun Bun <Aaronrls1984@gmail.com>
2022-11-07 11:42:59 -0500

    soo close

--- client/src/components/post-list.component.js
+++ client/src/components/post-list.component.js
@@ -13,7 +13,7 @@ export default class PostList extends Component {
   }
   componentDidMount() {
     if (process.env.NODE_ENV === 'production') {
-      axios.get('/posts/' )
+      axios.get('posts/' )
       .then(res => {
         this.setState({
           posts: res.data
